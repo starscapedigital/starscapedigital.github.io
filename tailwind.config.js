b167ae9b4874742f5566d1973584394c063c0b6c
Lavitz Star <astralvitz@pm.me>
2023-06-03 23:38:55 -0400

    The Birth

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
     extend: {
       fontFamily: {
         moonstar: ['Moonstar', 'Decoder', 'sans-serif'],
